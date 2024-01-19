bc280954518641b8eb9525b79f2e7e1a4a5a5205
JoyTaribagshaw <joytaribagshaw@gmail.com>
2024-01-19 23:20:28 +0000

    create and style About Me section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe(){
+  return (
+    <section id="AboutMe" className="about-section">
+      <div className="about-section-img">
+<img src="src/assets/-bsb65x.jpg" alt="About Me" />
+      </div>
+      <div className="hero-section-content about-section-box">
+<div className="hero-section-content">
+  <p className="section-title">About</p>
+  <h1 className="skills-section-heading">About Me</h1>
+  <p className="hero-section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, sunt nostrum quam expedita aperiam dolores consectetur iusto fuga reiciendis dolore aspernatur atque quod quidem, id tempore, vel commodi. Culpa, laudantium?</p>
+  <p className="hero-section-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+</div>
+      </div>
+    </section>
+  )
+}
